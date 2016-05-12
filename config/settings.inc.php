f1d0f95d2512a36a0071fce4498351fe4c113d6f
Ariadna <rockerysshop@gmail.com>
2016-05-12 20:42:22 +0200

    "Primer commit version 2"

---
+++ config/settings.inc.php
@@ -0,0 +1,16 @@
+<?php
+define('_DB_SERVER_', 'localhost');
+define('_DB_NAME_', 'rockeryshop');
+define('_DB_USER_', 'root');
+define('_DB_PASSWD_', '41002590f');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
+define('_PS_CACHE_ENABLED_', '0');
+define('_COOKIE_KEY_', 'nfnqudWcoccawacKhT8NjugB3eaOxYRIDb8mJdYPuxGdr7ZziIp4AzDa');
+define('_COOKIE_IV_', 'Gyqjo1y7');
+define('_PS_CREATION_DATE_', '2016-04-13');
+if (!defined('_PS_VERSION_'))
+	define('_PS_VERSION_', '1.6.1.5');
+define('_RIJNDAEL_KEY_', 'E1WYHQTggtpQfj67ssm3yytDJKQ0ngef');
+define('_RIJNDAEL_IV_', 'QpKIqAruBAGv4/4t0ILoMQ==');
